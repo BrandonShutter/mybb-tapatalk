d79bd5eecec1a4901e5cbfa51973353792834314
Brandon Shutter <brandon@brandonshutter.com>
2020-04-28 11:24:09 -0600

    fix emoji path

--- mobiquo/emoji/emoji.class.php
+++ mobiquo/emoji/emoji.class.php
@@ -1,5 +1,5 @@
 <?php
-require_once MYBB_ROOT.'/mobiquo/emoji/emoji.class.php';
+require_once MYBB_ROOT.'/mobiquo/emoji/emoji.php';
 class tapatalkEmoji
 {
 	static public function covertEmojiToName($data) {   	
